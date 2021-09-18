ca78b261f85225159dcd5e20a184c6bc8f01e49e
Mbtt107 <harddeath13@gmail.com>
2021-09-18 09:11:16 +0300

    Add patch to define prebuilt kernel location
    Signed-off-by: Mbtt107 <harddeath13@gmail.com>

--- patcher.sh
+++ patcher.sh
@@ -7,12 +7,12 @@
 cd ..
 
 echo "Applying OBB fix"
-cd frameworks/native
-git remote add hirqiz https://github.com/DevOtag-Open-Source/android_frameworks_native
-git fetch hirqiz eleven
-git cherry-pick 9465dde
+#cd frameworks/native
+#git remote add hirqiz https://github.com/DevOtag-Open-Source/android_frameworks_native
+#git fetch hirqiz eleven
+#git cherry-pick 9465dde
 
-cd ../..
+#cd ../..
 
 echo "Copying prebuilt files and patches"
 cp -r GM9PRO_Sprout_Patcher/prebuilt device/GM/GM9PRO_sprout/prebuilt
@@ -25,6 +25,7 @@ git apply patches/0001-Force-prebuilt.patch
 git apply patches/0002-GM9PRO_sprout-Add-prebuilt-kernel.patch
 git apply patches/0003-GM9PRO_sprout-Redefine-wifi-module-path.patch
 git apply patches/0001-Copy-wlan-module-to-vendor-due-to-prebuilt-kernel.patch
+git apply patches/0001-Define-prebuilt-kernel-path.patch
 
 echo "Exiting in 5 seconds."
 read -n 1 -t 5
